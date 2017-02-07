af5b06b288e1f8924c1d5cfd7b11f4149c5d569a
Ayele <sayele@campus.tue.nl>
2017-02-07 15:58:44 +0100

    test signalr R

---
+++ WebApiTest/WebAPIDemo/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(WebAPIDemo.Startup))]
+
+namespace WebAPIDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+            //app.MapSignalR();
+
+            app.Map("/signalr", map =>
+            {
+                map.UseCors(CorsOptions.AllowAll);
+                var hubConfiguration = new HubConfiguration { };
+                map.RunSignalR(hubConfiguration);
+            });
+        }
+    }
+}
